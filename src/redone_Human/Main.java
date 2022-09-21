e1cd29565c8ae722ddb9bc911abf3281b8aea0fa
VitaliyKlochko <89vklochko89@gmail.com>
2022-09-21 22:58:14 +0300

    Begin+hw1+hw2

---
+++ src/redone_Human/Main.java
@@ -0,0 +1,25 @@
+package redone_Human;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+
+        Human h1 = new Human("Максим","Минск",35,"бренд-менеджера");
+        h1.result();
+
+        System.out.println();
+
+        Human h2 = new Human("Анна","Москва",29,"методиста образовательных программ");
+        h2.result();
+
+        System.out.println();
+
+        Human h3 = new Human("Катя","Калининград",28,"продакт-менеджера");
+        h3.result();
+
+        System.out.println();
+
+        Human h4 = new Human("Артем","Москва",27,"директора по развитию бизнеса");
+        h4.result();
+    }
+}
